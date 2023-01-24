f9d1396089bdf64895ead801f6df8d58c2b66d7d
한규진 <david0218@naver.com>
2023-01-24 20:33:23 +0900

    chore : 이런저런 theme config

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
+  useNextSeoProps() {
+    const { route } = useRouter();
+    if (route !== "/") {
+      return {
+        titleTemplate: "%s – 9yujin",
+      };
+    }
+  },
   logo: <span>9yujin</span>,
   project: {
     link: "https://github.com/9yujin",
@@ -18,10 +27,26 @@ const config: DocsThemeConfig = {
   search: {
     placeholder: "검색",
   },
-  docsRepositoryBase: "https://github.com/9yujin/nextra-docs-for-blog",
-  footer: {
-    text: "revised 23.01.24 | 한규진",
+  docsRepositoryBase:
+    "https://github.com/9yujin/nextra-docs-for-blog/blob/main",
+  editLink: {
+    text: "Github에서 편집하기",
   },
+  feedback: {
+    content: null,
+  },
+  footer: {
+    text: (
+      <span>
+        {new Date().getFullYear()} ©{" "}
+        <a href="https://nextra.site" target="_blank">
+          한규진
+        </a>
+        .
+      </span>
+    ),
+  },
+  gitTimestamp: null,
 };
 
 export default config;
